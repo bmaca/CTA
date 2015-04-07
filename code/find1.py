a1a523e624c416cca079ee5974cf2ef61f157f49
bmaca <novmaca@gmail.com>
2015-04-06 22:28:36 -0500

    Added some code that will fetch for route 22

---
+++ code/find1.py
@@ -0,0 +1,22 @@
+from xml.etree.ElementTree import parse
+import urllib
+
+u = urllib.urlopen('http://ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=22')
+data = u.read()
+f = open('rt22.xml', 'wb')
+f.write(data)
+f.close()
+
+doc = parse('rt22.xml')
+
+latitude = 41.98062
+longitude = -87.668452
+
+for bus in doc.findall('bus'):
+	lat = float(bus.findtext('lat'))
+	if lat > latitude:
+		direction = bus.findtext('d')
+		if direction.startswith('North'):
+			busid = bus.findtext('id')
+			print "BudId:",busid
+			print "lat:", lat
