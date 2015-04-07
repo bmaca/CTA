c4f5ca7dda9ab91ac4f2d48912c6885cabf46bc0
bmaca <novmaca@gmail.com>
2015-04-06 22:46:58 -0500

    changed file

--- code/find22.py
+++ code/find22.py
@@ -1,22 +1,22 @@
 from xml.etree.ElementTree import parse
 import urllib
 
-u = urllib.urlopen('http://ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=22')
+u = urllib.urlopen('http://ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=126')
 data = u.read()
-f = open('rt22.xml', 'wb')
+f = open('rt126.xml', 'wb')
 f.write(data)
 f.close()
 
-doc = parse('rt22.xml')
+doc = parse('rt126.xml')
 
 latitude = 41.879875
 longitude = -87.628833
 
 for bus in doc.findall('bus'):
-	lat = float(bus.findtext('lat'))
-	if lat > latitude:
+	lon = float(bus.findtext('lon'))
+	if lon > longitude:
 		direction = bus.findtext('d')
-		if direction.startswith('North'):
+		if direction.startswith('West'):
 			busid = bus.findtext('id')
 			print "BudId:",busid
-			print "lat:", lat
+			print "long:", lon
