dcbbeafc06f8a1d547af4b750682b51459207c51
bmaca <novmaca@gmail.com>
2015-04-10 00:00:04 -0500

    Added a while loop and a function to it

--- code/find126.py
+++ code/find126.py
@@ -1,5 +1,6 @@
 from xml.etree.ElementTree import parse
 import urllib
+import time
 
 u = urllib.urlopen('http://ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=126')
 data = u.read()
@@ -12,11 +13,22 @@ doc = parse('rt126.xml')
 latitude = 41.879875
 longitude = -87.628833
 
-for bus in doc.findall('bus'):
-	lon = float(bus.findtext('lon'))
-	if lon > longitude:
-		direction = bus.findtext('d')
-		if direction.startswith('West'):
-			busid = bus.findtext('id')
+def monitor_bus():
+	for bus in doc.findall('bus'):
+		lon = float(bus.findtext('lon'))
+		if lon > longitude:
+			direction = bus.findtext('d')
+			if direction.startswith('West'):
+				busid = bus.findtext('id')
 			print "BudId:",busid
 			print "long:", lon
+
+print '*' *10
+
+stop = 8
+		
+while stop >0:
+	monitor_bus()
+	time.sleep(1)
+
+	stop = stop -1
