9b3bab21781c7978fe81b4b0d476afd9c6a6b6f2
bmaca <novmaca@gmail.com>
2015-06-19 19:55:45 -0500

    Added another route for the divvy bikes

--- Divy_Bike/divy.py
+++ Divy_Bike/divy.py
@@ -36,8 +36,21 @@ def fetch_bikes_train():
                 print ('Available Bikes: {0}'.format( data['availableBikes']))
                 continue
 
+def fetch_another_closer_route():
+    if json_object['stationBeanList'] == []:
+        print 'I am sorry there is no data.'
+    else:
+        for data in json_object['stationBeanList']:
+            if data['id'] == 49:
+                print ('Street Address: {0}' .format(data['stAddress1']))
+                print ('Total Docks: {0}'.format(data['totalDocks']))
+                print ('Available Docks: {0}'.format(data['availableDocks']))
+                print ('Available Bikes: {0}'.format( data['availableBikes']))
+                continue
 
 
 fetch_bikes_sprout()
 print '--------------'
 fetch_bikes_train()
+print '--------------'
+fetch_another_closer_route()
