e5b02709f6b181f6d7736f42ba80dfc93dfe3a9d
bmaca <novmaca@gmail.com>
2015-06-30 00:15:15 -0500

    Added in a parameter to the function and took out some unnecessary blob, in order to make it more clean and managable

--- Divy_Bike/divy.py
+++ Divy_Bike/divy.py
@@ -8,52 +8,32 @@ response = requests.get(url)
 json_object = response.json()
 
 time_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+print "-" * 20
+print "DATE        TIME"
 print time_date
+print "-" * 20
 
-print '----------------'
-
-def fetch_bikes_sprout():
-    if json_object['stationBeanList'] == []:
-        print 'I am sorry there is no data'
-    else:
-        for data in json_object['stationBeanList']:
-            if data['id'] == 37:
-                print ('Street Address: {0}' .format(data['stAddress1']))
-                print ('Total Docks: {0}'.format(data['totalDocks']))
-                print ('Available Docks: {0}'.format(data['availableDocks']))
-                print ('Available Bikes: {0}'.format( data['availableBikes']))
-                print ('In Service: {0}' .format( data['statusValue']))
-                continue
-
-def fetch_bikes_train():
+def station_info(station_id):
     if json_object['stationBeanList'] == []:
         print 'I am sorry there is no data.'
     else:
         for data in json_object['stationBeanList']:
-            if data['id'] == 192:
-                print ('Street Address: {0}' .format(data['stAddress1']))
+            if data['id'] == station_id:
+                print ('Street Address: {0}'.format(data['stAddress1']))
                 print ('Total Docks: {0}'.format(data['totalDocks']))
                 print ('Available Docks: {0}'.format(data['availableDocks']))
                 print ('Available Bikes: {0}'.format( data['availableBikes']))
-                print ('In Service: {0}' .format( data['statusValue']))
-                continue
+                print ('In Service: {0}'.format( data['statusValue']))
 
-def fetch_another_closer_route():
-    if json_object['stationBeanList'] == []:
-        print 'I am sorry there is no data.'
-    else:
-        for data in json_object['stationBeanList']:
-            if data['id'] == 49:
-                print ('Street Address: {0}' .format(data['stAddress1']))
-                print ('Total Docks: {0}'.format(data['totalDocks']))
-                print ('Available Docks: {0}'.format(data['availableDocks']))
-                print ('Available Bikes: {0}'.format( data['availableBikes']))
-                print ('In Service: {0}' .format( data['statusValue']))
-                continue
+                
+if __name__ == '__main__':
+    print "-" * 20
+    station_info(37)
+    print "-" * 20
+    station_info(192)
+    print "-" * 20
+    station_info(49)
 
-
-fetch_bikes_sprout()
-print '--------------'
-fetch_bikes_train()
-print '--------------'
-fetch_another_closer_route()
+    
+    
